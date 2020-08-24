a9e3aeeb7035e06349f4c37aec4854baa00791e6
Charles Severance <csev@umich.edu>
2020-08-24 14:39:21 -0400

    Updates

--- database.php
+++ database.php
@@ -19,7 +19,7 @@ $buckets .= ",\n";
 $DATABASE_INSTALL = array(
 
   array( "{$CFG->dbprefix}youtube_views",
-  "CREATE TABLE `{$CFG->dbprefix}youtube_views` (
+  "CREATE TABLE {$CFG->dbprefix}youtube_views (
     link_id             INTEGER NOT NULL,
     seconds             INTEGER NOT NULL,
     width               INTEGER NOT NULL,
@@ -29,9 +29,9 @@ $DATABASE_INSTALL = array(
 
 ".$buckets."
 
-    CONSTRAINT `{$CFG->dbprefix}youtube_views_ibfk_1`
-        FOREIGN KEY (`link_id`)
-        REFERENCES `{$CFG->dbprefix}lti_link` (`link_id`)
+    CONSTRAINT {$CFG->dbprefix}youtube_views_ibfk_1
+        FOREIGN KEY (link_id)
+        REFERENCES {$CFG->dbprefix}lti_link (link_id)
         ON DELETE CASCADE ON UPDATE CASCADE,
 
     UNIQUE(link_id)
@@ -40,7 +40,7 @@ $DATABASE_INSTALL = array(
   "),
 
   array( "{$CFG->dbprefix}youtube_views_user",
-  "CREATE TABLE `{$CFG->dbprefix}youtube_views_user` (
+  "CREATE TABLE {$CFG->dbprefix}youtube_views_user (
     link_id             INTEGER NOT NULL,
     user_id             INTEGER NOT NULL,
     seconds             INTEGER NOT NULL,
@@ -51,14 +51,14 @@ $DATABASE_INSTALL = array(
 
 ".$buckets."
 
-    CONSTRAINT `{$CFG->dbprefix}youtube_views_user_ibfk_1`
-        FOREIGN KEY (`link_id`)
-        REFERENCES `{$CFG->dbprefix}lti_link` (`link_id`)
+    CONSTRAINT {$CFG->dbprefix}youtube_views_user_ibfk_1
+        FOREIGN KEY (link_id)
+        REFERENCES {$CFG->dbprefix}lti_link (link_id)
         ON DELETE CASCADE ON UPDATE CASCADE,
 
-    CONSTRAINT `{$CFG->dbprefix}youtube_views_user_ibfk_2`
-        FOREIGN KEY (`user_id`)
-        REFERENCES `{$CFG->dbprefix}lti_user` (`user_id`)
+    CONSTRAINT {$CFG->dbprefix}youtube_views_user_ibfk_2
+        FOREIGN KEY (user_id)
+        REFERENCES {$CFG->dbprefix}lti_user (user_id)
         ON DELETE CASCADE ON UPDATE CASCADE,
 
     UNIQUE(link_id, user_id)
